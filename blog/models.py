dea36c07d56a6287055cde12e77565338f54a101
Setul Kamleshbhai Patel <setul@Setuls-MacBook-Pro.local>
2020-11-21 00:05:41 -0500

    Course-Done

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,17 @@ from django.db import models
 
 class blog(models.Model):
     title = models.CharField(max_length=250)
-    pub_date = models.DateTimeField()
+    pub_date = models.DateField(auto_now=False, auto_now_add=False)
     body = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+    
+    def __str__(self):              #It will change blog name to the title of it in the database
+        return self.title
+
+    def pub_date_done(self):        #Changing the date form it was not good so..
+       return self.pub_date.strftime('%b %e %Y')
+
+   # def summary(self):
+   #     return self.body[:100]
+   
+   
--- jobs/models.py
+++ jobs/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 
 class job(models.Model):
+    title = models.CharField(max_length=100)
     image = models.ImageField(upload_to = 'images/')
     summary = models.TextField(max_length= 5000)
+    
+    def __str__(self):              #It will change blog name to the title of it in the database
+        return self.title
