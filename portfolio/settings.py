1fa22f2c48ff9f2b7855ac85d36f0a860bbdfea9
Setul Kamleshbhai Patel <setul@Setuls-MBP.phub.net.cable.rogers.com>
2021-01-17 22:00:04 -0500

    Added favicon

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER' : 'postgres',
+        'USER' : 'djangodbman',
         'PASSWORD' : 'Sp07121997$',
         'HOST' : 'localhost',
         'PORT' : '5432',
