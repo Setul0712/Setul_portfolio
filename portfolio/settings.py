1af94493e6fe80e060320e62e9fe93a04667c68b
Setul Kamleshbhai Patel <setul@Setuls-MBP.phub.net.cable.rogers.com>
2021-01-17 21:49:36 -0500

    Added favicon

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER' : 'djangodbman',
-        'PASSWORD' : 'Sp07121997',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Sp07121997$',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
@@ -134,6 +134,6 @@ MEDIA_URL= '/media/'
 
 
 try:
-    from .local_settings import *
+    from local_settings import *
 except ImportError:
     pass
